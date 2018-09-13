5d59b5ffdbd51e327bcf103eaea102413e068cf5
andriy bondar <andriy.bondar@coconutsoftware.com>
2018-09-13 14:47:27 -0600

    adding javascripting and files to git

---
+++ javascripting/if-statement.js
@@ -0,0 +1,15 @@
+var fruit = 'orange';
+fruit.lenght;
+
+	if (fruit > 5){
+	console.log ('The fruit name has more than five characters');
+		} else {
+		console.log('The fruit name is five characrers or less.');
+}
+if (fruit.length > 5){
+} console.log('')
+else {console.log()
+}
+
+
+if (fruitLenght > 5)
---
+++ javascripting/rounding-numbers.js
@@ -0,0 +1,6 @@
+var roundUp = 1.5;
+roundUp = roundUp.Math.round(0.5);
+
+var rounded = Math 'roundUp';
+console.log (rounded);
+
